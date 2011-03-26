9b21490486ac9c0977d0b11cfe8078904a3cb2df
Your Name <james@r0bertson.co.uk>
2011-03-26 18:18:42 +0000

    bug

--- chronic_between.gemspec
+++ chronic_between.gemspec
@@ -1,6 +1,7 @@
 Gem::Specification.new do |s|
   s.name = 'chronic_between'
-  s.version = '0.1.0'
+  s.version = '0.1.4'
   s.summary = 'chronic_between'
   s.files = Dir['lib/**/*.rb']
+  s.add_dependency('chronic')
 end
--- lib/chronic_between.rb
+++ lib/chronic_between.rb
@@ -2,6 +2,9 @@
 
 # file: chronic_between.rb
 
+require 'chronic'
+require 'date'
+
 class ChronicBetween
 
   def initialize(s)
@@ -24,7 +27,7 @@ class ChronicBetween
 
         d1, d2, rt1, rt2 = raw1.captures
         date_ranges << [[d1,rt1], [d2,rt2]].map do |d,r| 
-          cdate = Chronic.parse(d, :now => (date - 1)
+          cdate = Chronic.parse(d, :now => (date - 1))
           DateTime.parse(cdate).strftime("%d-%b-%y") + ' ' + r)
         end
 
